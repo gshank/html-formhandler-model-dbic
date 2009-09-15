6c68d49eec51199e46a459a9865b6bb56245c17d
Gerda Shank <gerda.shank@gmail.com>
2009-09-15 14:03:30 -0400

    bump version and RU prereq

--- lib/HTML/FormHandler/Model/DBIC.pm
+++ lib/HTML/FormHandler/Model/DBIC.pm
@@ -7,7 +7,7 @@ use DBIx::Class::ResultClass::HashRefInflator;
 use DBIx::Class::ResultSet::RecursiveUpdate;
 use Scalar::Util qw(blessed);
 
-our $VERSION = '0.04002';
+our $VERSION = '0.05';
 
 =head1 NAME
 
