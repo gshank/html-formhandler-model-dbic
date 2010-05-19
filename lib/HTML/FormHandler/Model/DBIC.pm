29e96dd3ed56f00e3ccaf42914607a8f337dd3eb
Gerda Shank <gerda.shank@gmail.com>
2010-05-18 20:40:58 -0400

    to 0.11

--- lib/HTML/FormHandler/Model/DBIC.pm
+++ lib/HTML/FormHandler/Model/DBIC.pm
@@ -7,7 +7,7 @@ use DBIx::Class::ResultClass::HashRefInflator;
 use DBIx::Class::ResultSet::RecursiveUpdate;
 use Scalar::Util ('blessed');
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 NAME
 
